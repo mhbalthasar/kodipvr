eb8997ec70987724dcebcbf27195e0efccf64471
scskarsper <scskarsper@qq.com>
2022-02-06 12:04:18 +0800

    1

---
+++ shell.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+mkdir src
+git clone https://github.com.cnpmjs.org/aowatchsea/Aran
+cp ./Aran/all.m3u ./src/src1.m3u8
+git clone https://github.com.cnpmjs.org/SPX372928/MyIPTV
+cp ./MyIPTV/上海百视通联通ip版.txt ./src/百事通联通.txt
+cp ./MyIPTV/辽宁PLTV移动ip不完整版.txt ./src/辽宁移动.txt
+
+cat ./src/src1.m3u8 > ./result.m3u8
+echo -e "\r\n" >> ./result.m3u8
+cat ./src/custom.m3u8 >> ./result.m3u8
+rm -rf ./Aran
+rm -rf ./MyIPTV
