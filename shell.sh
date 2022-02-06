384f00207c20e0b22b283b08e5b0e48f6b0073b1
mhbalthasar <98707331+mhbalthasar@users.noreply.github.com>
2022-02-06 12:22:23 +0800

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 mkdir src
+mkdir src/txt
 git clone https://github.com.cnpmjs.org/aowatchsea/Aran
 cp ./Aran/all.m3u ./src/src1.m3u8
 git clone https://github.com.cnpmjs.org/SPX372928/MyIPTV
-cp ./MyIPTV/上海百视通联通ip版.txt ./src/百事通联通.txt
-cp ./MyIPTV/北方广电辽宁有线CDN版.txt ./src/辽宁有线.txt
+cp ./MyIPTV/*.txt ./src/txt/
+rm ./src/txt/Y失效*.txt
 
 cat ./src/src1.m3u8 > ./result.m3u8
 echo -e "\r\n" >> ./result.m3u8
